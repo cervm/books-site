0fe3f05bd854b387f6b8bc21d64a5ac338f5c810
Marek Cervinka <marek@zliide.com>
2019-12-20 12:12:22 +0000

    Implement Add Book component

---
+++ client/src/AddBook.js
@@ -0,0 +1,105 @@
+import React, {Component} from 'react';
+
+export default class AddBook extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            title: "",
+            author: "",
+            categoryAlias: "",
+            price: 0,
+        };
+        this.handleInput = this.handleInput.bind(this);
+        this.onChange = this.onChange.bind(this);
+    }
+
+    handleInput(event) {
+        event.preventDefault();
+        this.props.onAddBook(
+            this.state.title,
+            this.state.author,
+            this.getCategory(this.state.categoryAlias),
+            this.state.price,
+            {
+                "email": "john.snow@gmail.com",
+                "name": "John Snow"
+            },
+        );
+        this.setState({title: ""})
+    }
+
+    getCategory(alias) {
+        return this.props.categories.find(category => category.alias === alias);
+    }
+
+    onChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    render() {
+        return (
+            <form>
+                <div className="field">
+                    <label className="label" htmlFor="TitleInput">Title</label>
+                    <div className="control">
+                        <input className="input" type="text"
+                               onChange={this.onChange}
+                               name="title"
+                               value={this.state.title}
+                               placeholder="Title"
+                               id="TitleInput"
+                        />
+                    </div>
+                </div>
+                <div className="field">
+                    <label className="label" htmlFor="AuthorInput">Author</label>
+                    <div className="control">
+                        <input className="input" type="text"
+                               onChange={this.onChange}
+                               name="author"
+                               value={this.state.author}
+                               placeholder="Author"
+                               id="AuthorInput"
+                        />
+                    </div>
+                </div>
+                <div className="field">
+                    <label className="label" htmlFor="CategorySelect">Category</label>
+                    <div className="control">
+                        <div className="select">
+                            <select onChange={this.onChange}
+                                    name="categoryAlias"
+                                    value={this.state.categoryAlias}
+                                    id="CategorySelect">
+                                {this.props.categories.map(category =>
+                                    <option key={category.alias} value={category.alias}>{category.name}</option>
+                                )}
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div className="field">
+                    <label className="label" htmlFor="PriceInput">Price</label>
+                    <div className="control">
+                        <input className="input" type="number" min="0"
+                               onChange={this.onChange}
+                               name="price"
+                               value={this.state.price}
+                               placeholder="Price"
+                               id="PriceInput"
+                        />
+                    </div>
+                </div>
+                <div className="field">
+                    <button className="button is-primary" onClick={this.handleInput} type="submit"
+                            id="QuestionButton">Add Book
+                    </button>
+                </div>
+            </form>
+        )
+    };
+}
--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,36 @@
 import React, {Component} from 'react';
 import {Link, Router} from "@reach/router";
+
 import Book from "./Book";
 import Books from "./Books";
+import AddBook from "./AddBook";
+import UserHeader from "./UserHeader";
+import Alert from "./Alert";
+import Login from "./Login";
 
 // Connect wraps a component and makes it a Container component
 import {connect} from "react-redux";
-import {postBook, fetchBooks} from "./actions";
+import {login, logout, postBook, fetchBooks, postCategory, fetchCategories, hideAlert} from "./actions";
+import Categories from "./Categories";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            alertMsg: ""
+        };
+    }
+
     componentDidMount() {
+        this.props.fetchCategories();
         this.props.fetchBooks();
+    }
+
+    resetAlert() {
+        this.setState({
+            alertMsg: "",
+            suppressInfo: true
+        })
     }
 
     // We keep this method for searching in the list of books provided to the component
@@ -17,10 +38,36 @@ class App extends Component {
         return this.props.books.find(book => book.id === id);
     }
 
+    getBooksInCategory(alias) {
+        return this.props.books.filter(book => book.category.alias === alias);
+    }
+
     render() {
+        let notification = <></>;
+        if (this.props.notifications.active) {
+            const notif = this.props.notifications;
+            const level = notif.level === "error" ? "is-danger" : "is-warning";
+
+            notification = <section className={`hero ${level} is-small`}>
+                <div className="hero-body">
+                    <div className="container">
+                        <button onClick={() => this.props.hideAlert()} className="delete is-large is-pulled-right"/>
+                        <h1 className="title">
+                            {notif.title}
+                        </h1>
+                        <h2 className="subtitle">
+                            {notif.text}
+                        </h2>
+                    </div>
+                </div>
+            </section>
+        }
+
         return (
             <>
-                <section className="hero is-primary">
+                {notification}
+
+                <section className="hero is-primary is-bold">
                     <div className="hero-body">
                         <div className="container">
                             <Link to="/"><h1 className="title is-2">Books Marketplace</h1></Link>
@@ -30,15 +77,36 @@ class App extends Component {
                         </div>
                     </div>
                 </section>
-                <section>
+
+                <UserHeader username={this.props.user.username} logout={_ => this.props.logout()}/>
+
+                <section className="section">
+                    <Alert msg={this.state.alertMsg}/>
+
                     <div className="container">
                         <Router>
                             <Book path="/books/:id" getBook={id => this.getBook(id)}/>
-                            <Books path="/" books={this.props.books}
-                                   addBook={title => this.props.postBook(title)}/>
+
+                            <Categories path="/" categories={this.props.categories}/>
+
+                            <Books path="/books" getBooks={_ => this.props.books}/>
+
+                            <Books path="/books/category/:alias" getBooks={alias => this.getBooksInCategory(alias)}/>
+
+                            <AddBook path="/books/add"
+                                     categories={this.props.categories}
+                                     onAddBook={(title, author, category, price, seller) =>
+                                         this.props.postBook(title, author, category, price, seller)}
+                            />
+
+                            <Login path="/login"
+                                   login={(username, password) => this.props.login(username, password)}
+                                   infoMsg={this.state.infoMsg}
+                            />
                         </Router>
                     </div>
                 </section>
+                <Link to="/books/add">Add Book</Link>
                 <footer className="footer">
                     <div className="container">
                         <div className="content has-text-centered">
@@ -55,13 +123,21 @@ class App extends Component {
 
 // Mapping our state to props for <App>
 const mapStateToProps = state => ({
-    books: state.books
+    categories: state.categories,
+    books: state.books,
+    user: state.user,
+    notifications: state.notifications,
 });
 
 // Making callbacks available to <App>
 const mapDispatchToProps = dispatch => ({
-    postBook: (title) => dispatch(postBook(title)),
-    fetchBooks: _ => dispatch(fetchBooks())
+    postBook: (title, author, category, price, seller) => dispatch(postBook(title, author, category, price, seller)),
+    fetchBooks: (categoryAlias) => dispatch(fetchBooks(categoryAlias)),
+    postCategory: (alias, name) => dispatch(postCategory(alias, name)),
+    fetchCategories: _ => dispatch(fetchCategories()),
+    login: (username, password) => dispatch(login(username, password)),
+    logout: _ => dispatch(logout()),
+    hideAlert: _ => dispatch(hideAlert())
 });
 
 // Wrapping <App> and exporting the wrapped component instead of the original component
