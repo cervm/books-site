01fc35ddc78f09b1c736dd88409e3cba6ba9c03f
Marek Cervinka <marek@zliide.com>
2019-12-20 13:34:17 +0000

    Redirect to login page if not logged in

--- client/src/Admin.js
+++ client/src/Admin.js
@@ -1,23 +1,27 @@
 import React, {Component} from 'react';
-import {Link} from "@reach/router";
+import {Link, Redirect} from "@reach/router";
 import CreateCategory from "./CreateCategory";
 
 class Admin extends Component {
     render() {
-        return (
-            <React.Fragment>
-                <h2 className="title is-2">Manage Categories</h2>
-                <ul>
-                    {this.props.categories.map(category =>
-                        <li key={category.alias}>
-                            <Link to={`/books/category/${category.alias}`}>{category.name}</Link>
-                        </li>
-                    )}
-                </ul>
-                <br/>
-                <CreateCategory onCreateCategory={this.props.onCreateCategory}/>
-            </React.Fragment>
-        );
+        if (this.props.username) {
+            return (
+                <React.Fragment>
+                    <h2 className="title is-2">Manage Categories</h2>
+                    <ul>
+                        {this.props.categories.map(category =>
+                            <li key={category.alias}>
+                                <Link to={`/books/category/${category.alias}`}>{category.name}</Link>
+                            </li>
+                        )}
+                    </ul>
+                    <br/>
+                    <CreateCategory onCreateCategory={this.props.onCreateCategory}/>
+                </React.Fragment>
+            );
+        } else {
+            return <Redirect to="/login"/>
+        }
     }
 
 }
