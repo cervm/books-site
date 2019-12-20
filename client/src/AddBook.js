25142db5d1c09f776964762de5cf73930e9ca085
Marek Cervinka <marek@zliide.com>
2019-12-20 13:46:03 +0000

    Remove broken redirects

--- client/src/AddBook.js
+++ client/src/AddBook.js
@@ -47,70 +47,65 @@ export default class AddBook extends Component {
     }
 
     render() {
-        if (this.props.username) {
-
-            return (
-                <form>
-                    <div className="field">
-                        <label className="label" htmlFor="TitleInput">Title</label>
-                        <div className="control">
-                            <input className="input" type="text"
-                                   onChange={this.onChange}
-                                   name="title"
-                                   value={this.state.title}
-                                   placeholder="Title"
-                                   id="TitleInput"
-                            />
+        return (
+            <form>
+                <div className="field">
+                    <label className="label" htmlFor="TitleInput">Title</label>
+                    <div className="control">
+                        <input className="input" type="text"
+                               onChange={this.onChange}
+                               name="title"
+                               value={this.state.title}
+                               placeholder="Title"
+                               id="TitleInput"
+                        />
+                    </div>
+                </div>
+                <div className="field">
+                    <label className="label" htmlFor="AuthorInput">Author</label>
+                    <div className="control">
+                        <input className="input" type="text"
+                               onChange={this.onChange}
+                               name="author"
+                               value={this.state.author}
+                               placeholder="Author"
+                               id="AuthorInput"
+                        />
+                    </div>
+                </div>
+                <div className="field">
+                    <label className="label" htmlFor="CategorySelect">Category</label>
+                    <div className="control">
+                        <div className="select">
+                            <select onChange={this.onChange}
+                                    name="categoryAlias"
+                                    value={this.state.categoryAlias}
+                                    id="CategorySelect">
+                                {this.props.categories.map(category =>
+                                    <option key={category.alias} value={category.alias}>{category.name}</option>
+                                )}
+                            </select>
                         </div>
                     </div>
-                    <div className="field">
-                        <label className="label" htmlFor="AuthorInput">Author</label>
-                        <div className="control">
-                            <input className="input" type="text"
-                                   onChange={this.onChange}
-                                   name="author"
-                                   value={this.state.author}
-                                   placeholder="Author"
-                                   id="AuthorInput"
-                            />
-                        </div>
+                </div>
+                <div className="field">
+                    <label className="label" htmlFor="PriceInput">Price</label>
+                    <div className="control">
+                        <input className="input" type="number" min="0"
+                               onChange={this.onChange}
+                               name="price"
+                               value={this.state.price}
+                               placeholder="Price"
+                               id="PriceInput"
+                        />
                     </div>
-                    <div className="field">
-                        <label className="label" htmlFor="CategorySelect">Category</label>
-                        <div className="control">
-                            <div className="select">
-                                <select onChange={this.onChange}
-                                        name="categoryAlias"
-                                        value={this.state.categoryAlias}
-                                        id="CategorySelect">
-                                    {this.props.categories.map(category =>
-                                        <option key={category.alias} value={category.alias}>{category.name}</option>
-                                    )}
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="field">
-                        <label className="label" htmlFor="PriceInput">Price</label>
-                        <div className="control">
-                            <input className="input" type="number" min="0"
-                                   onChange={this.onChange}
-                                   name="price"
-                                   value={this.state.price}
-                                   placeholder="Price"
-                                   id="PriceInput"
-                            />
-                        </div>
-                    </div>
-                    <div className="field">
-                        <button className="button is-primary" onClick={this.handleInput} type="submit"
-                                id="AddBookButton">Add Book
-                        </button>
-                    </div>
-                </form>
-            )
-        } else {
-            return <Redirect to="/login"/>
-        }
+                </div>
+                <div className="field">
+                    <button className="button is-primary" onClick={this.handleInput} type="submit"
+                            id="AddBookButton">Add Book
+                    </button>
+                </div>
+            </form>
+        )
     };
 }
--- client/src/Admin.js
+++ client/src/Admin.js
@@ -4,24 +4,20 @@ import CreateCategory from "./CreateCategory";
 
 class Admin extends Component {
     render() {
-        if (this.props.username) {
-            return (
-                <React.Fragment>
-                    <h2 className="title is-2">Manage Categories</h2>
-                    <ul>
-                        {this.props.categories.map(category =>
-                            <li key={category.alias}>
-                                <Link to={`/books/category/${category.alias}`}>{category.name}</Link>
-                            </li>
-                        )}
-                    </ul>
-                    <br/>
-                    <CreateCategory onCreateCategory={this.props.onCreateCategory}/>
-                </React.Fragment>
-            );
-        } else {
-            return <Redirect to="/login"/>
-        }
+        return (
+            <React.Fragment>
+                <h2 className="title is-2">Manage Categories</h2>
+                <ul>
+                    {this.props.categories.map(category =>
+                        <li key={category.alias}>
+                            <Link to={`/books/category/${category.alias}`}>{category.name}</Link>
+                        </li>
+                    )}
+                </ul>
+                <br/>
+                <CreateCategory onCreateCategory={this.props.onCreateCategory}/>
+            </React.Fragment>
+        );
     }
 
 }
